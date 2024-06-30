05387ab233f68489941a215325cb773599b2df25
Alona Kuznietsova <acvetochka@gmail.com>
2024-06-30 20:57:21 +0200

    change modal

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,5 @@
+let isModalOpen = false;
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -7,11 +9,36 @@
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", onBackdropClick)
 
    
 
   function toggleModal() {
     document.body.classList.toggle("modal-open");
     refs.modal.classList.toggle("backdrop--hidden");
+    isModalOpen = !isModalOpen;
   }
+
+  window.addEventListener('keyup', onKeyUp);
+
+  function onKeyUp(e) {
+    e.preventDefault();
+
+    if(isModalOpen) {
+      if (e.code === 'Escape') {
+        toggleModal();
+      }
+    }
+
+  }
+
+  function onBackdropClick(e) {
+    if (e.target.classList.contains('backdrop')) {
+      toggleModal();
+    }
+  }
+
 })();
+
+
+
